8bc25362183dded464115d0ae607818062b9bf67
Jemal Ahmed <ahmed.jmal@dksoft.tn>
2023-10-30 19:23:10 +0100

    configuration ngrok

--- src/main/java/com/formation/integcontinue/App.java
+++ src/main/java/com/formation/integcontinue/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "BOnjour !" );
+        System.out.println( "BOnjour tout le monde!" );
     }
 }
